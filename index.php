7d0e2342792043b2573f5dcd2026656d484543e6
Administrator <admin@example.com>
2023-06-23 22:43:51 +0200

    UPD index

--- index.php
+++ index.php
@@ -1,19 +1,37 @@
-<!doctype html>
-<html lang="fr">
-
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="./_css/style.css" rel="stylesheet">
-
-</head>
-
-<body>
-  <div class="bg">
-
-  </div>
-  <div class="br"></div>
-  <div class="bb"></div>
-  <div class="bn"></div>
-</body>
-
+<!doctype html>
+
+<html lang="fr">
+
+test
+
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="./_css/style.css" rel="stylesheet">
+
+
+
+</head>
+
+
+
+<body>
+
+  <div class="bg">
+
+
+
+  </div>
+
+  <div class="br"></div>
+
+  <div class="bb"></div>
+
+  <div class="bn"></div>
+
+</body>
+
+
+
 </html>
