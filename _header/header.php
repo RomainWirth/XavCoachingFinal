0e1dacea39828351cb9c8bdbe34a6f895a9a9644
Administrator <admin@example.com>
2023-06-23 22:38:39 +0200

    firstcommit

---
+++ _header/header.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="flex-row space-between">
+        <div>
+            <a href="#">
+                <img src="./_css/images/logos/XavCoachingLogo.png" alt="logo">
+            </a>
+        </div>
+        <div>
+            <ul class="flex-row justify-between">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Coucou c'est moi !</a></li>
+                <li><a href="#">Viens souffrir ici !</a></li>
+                <li><a href="#">T'en fais pas je suis gentil</a></li>
+            </ul>
+            <a href="#">Connection</a>
+        </div>
+    </nav>
+</header>
+
+<?php
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="./_css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <div class="bg">
+
+  </div>
+  <div class="br"></div>
+  <div class="bb"></div>
+  <div class="bn"></div>
+</body>
+
+</html>
