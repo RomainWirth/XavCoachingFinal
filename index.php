a2d2355ab7f2e62dd55be45d74aed51e5568e6bf
RomainWirth <wirth.romain@gmail.com>
2023-06-25 19:57:52 +0200

    Rom UPD index

--- index.php
+++ index.php
@@ -1,36 +1,21 @@
 <!doctype html>
-
 <html lang="fr">
 
-
-
 <head>
-
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <link href="./_css/style.css" rel="stylesheet">
-
-
-
 </head>
 
 
 
 <body>
-  <h1>SITE EN CONSTRUCTION</h1>
   <div class="bg">
     <?php include("_header/header.php"); ?>
+    <h1>SITE EN CONSTRUCTION</h1>
+
   </div>
-
-
   <div class="br"></div>
-  
   <div class="bb"></div>
-
   <div class="bn"></div>
-
 </body>
-
-
-
 </html>
